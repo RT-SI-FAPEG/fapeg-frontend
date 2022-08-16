5500ad50d030b3349367392242d2f04a95e6e07a
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-08-15 23:49:17 -0300

    feat: Added registration confirmation text

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  legalPersonForm: FormGroup
+  physicalPersonForm: FormGroup
+  makingRegister: boolean = true
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.legalPersonForm = this.formBuilder.group({
+
+    })
+
+    this.physicalPersonForm = this.formBuilder.group({
+      
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.makingRegister = false
   }
 
 }
