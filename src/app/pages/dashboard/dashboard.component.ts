9b4359c13848915a986f37a33c71259e94a9afb2
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-08-22 23:58:30 -0300

    feat: Integrate DashboardComponent with api indicators

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IndicatorsService } from 'src/app/services/indicators.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  indicators: any[] = []
+
+  constructor(
+    private indicatorsService: IndicatorsService
+  ) { }
 
   ngOnInit(): void {
+    this.indicatorsService.getIndicators()
+      .subscribe(
+        (indicators) => {
+          this.indicators = indicators
+        },
+        (err) => {
+          console.log(err)
+        }
+      )
   }
 
 }
