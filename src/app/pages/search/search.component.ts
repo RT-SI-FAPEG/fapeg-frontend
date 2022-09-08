20b9bfb0be6e8df55a2afb37a42a1c8769d926fb
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-09-08 19:36:33 -0300

    fix: fixed SeachComponent to correct file

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,2 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent implements OnInit {
+  search: any;
 
+  constructor(private router: Router) {
+    this.search = this.router.getCurrentNavigation()!.extras.state;
+  }
+
+  ngOnInit(): void {
+    console.log(this.search);
+  }
+}
